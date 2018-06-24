e3de2139b8dc496bd633650254f8683be3f7ce8d
Xavier <xavier.astor.fontaine@gmail.com>
2018-06-24 16:34:11 +0000

    Request and display working

--- application.py
+++ application.py
@@ -1,1 +1,25 @@
+import os
+import requests
 from flask  import Flask, render_template
+
+
+API_KEY =  os.environ.get("API_KEY")
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def get_index():
+    
+    r = requests.get('https://api.mlab.com/api/1/databases/github-tweets/collections/cybersecurity?apiKey=' + API_KEY)
+    request_content     = r.json()
+    request_content_1   = request_content[0]["text"]
+    return render_template('index.html', req = request_content_1)
+    
+    
+# ------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
+
+# ------------------------------------------------------------------------------
