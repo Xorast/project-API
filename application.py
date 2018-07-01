114be99b20e5089cdf1431b32af2676bd3ce2ac0
Xavier <xavier.astor.fontaine@gmail.com>
2018-07-01 21:32:37 +0000

    API News implemented

--- application.py
+++ application.py
@@ -1,32 +1,49 @@
 import  os
+import  json
 import  requests
 from    flask       import Flask, render_template
 
 
-API_KEY =  os.environ.get("API_KEY")
+API_KEY_1   = os.environ.get("API_KEY_1")
+API_NAME_1  = os.environ.get("API_NAME_1")
 
+API_KEY_2   = os.environ.get("API_KEY_2")
+API_NAME_2  = os.environ.get("API_NAME_2")
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_index():
     
-    r = requests.get('https://api.mlab.com/api/1/databases/github-tweets/collections/CyberSecurity?apiKey=' + API_KEY)
-    request_content     = r.json()
+    # --------------------------
+    url     = (API_NAME_2 + API_KEY_2)
     
-    count = []
+    r2       = requests.get(url)
+    r2       = r2.json()
+    
+    articles = r2["articles"]
+    print(r2["totalResults"])
+    
+    # --------------------------
+    
+    # --------------------------
+    r1              = requests.get(API_NAME_1 + API_KEY_1)
+    request_content = r1.json()
+    
+    tweets_array = []
     
     for tweet in request_content :
-        count.append({  "text"          : tweet['text'], 
-                        "likes"         : tweet['user']['favourites_count'],
-                        "username"      : tweet['user']['name'],
-                        "retweet_count" : tweet['retweet_count'],
-                        "reply_count"   : tweet['reply_count'],
-                        "retweeted"     : tweet['retweeted_status']['created_at'] if "retweeted_status" in tweet else "NO"
-                    })
+        tweets_array.append({   "text"          : tweet['text'], 
+                                "likes"         : tweet['user']['favourites_count'],
+                                "username"      : tweet['user']['name'],
+                                "retweet_count" : tweet['retweet_count'],
+                                "reply_count"   : tweet['reply_count'],
+                                "retweeted"     : tweet['retweeted_status']['created_at'] if "retweeted_status" in tweet else "NO"
+                            })
+                
+    # -------------------------
     
-    # request_content_1   = request_content[0]["text"]
-    return render_template('index.html', req = count)
+    return render_template('index.html', tweets = tweets_array, articles = articles)
     
     
 # ------------------------------------------------------------------------------
