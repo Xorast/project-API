9a57b5f91dd403131ed9f72a89b0bc8c3e8b0fdb
Xavier <xavier.astor.fontaine@gmail.com>
2018-10-17 00:17:11 +0000

    First layout. To be continued.

--- application.py
+++ application.py
@@ -1,7 +1,7 @@
 import  os
 import  json
-import  requests
-from    flask       import Flask, render_template
+
+from    flask       import Flask, render_template, request
 from    get_data    import get_news, get_tweets
 
 
@@ -21,12 +21,16 @@ def get_index():
     tweets    = get_tweets(API_NAME_1, API_KEY_1) 
     
     return render_template('index.html', tweets = tweets, articles = articles)
-    # return render_template('index.html', tweets = tweets)
     
-@app.route('/major_breachs')
-def display_data():
-    # breachs data displayed with dc/crossfilter/d3
-    return render_template('index.html')
+# @app.route('/')
+# def get_results():
+    
+#     userSearch = request.args.get('userSearch')
+    
+#     articles  = get_news(API_NAME_2, API_KEY_2)
+#     tweets    = get_tweets(API_NAME_1, API_KEY_1) 
+    
+#     return render_template('results.html', tweets = tweets, articles = articles)
     
 # ------------------------------------------------------------------------------
 
