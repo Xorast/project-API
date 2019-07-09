5921ea90899f350966de23dabf803c78ca9bb293
pangur <pangur@tutanota.com>
2019-07-09 23:37:53 +0100

    Single Page Application: code moved to front-end

--- application.py
+++ application.py
@@ -1,35 +1,13 @@
-import  os
-import  json
-from    flask   import Flask, render_template, request
+import os
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def get_index():
+    """ Single Page Application """
     return render_template('index.html')
 
 
-
-# FORMER CODE - MPA - Replaced by SPA version
-
-# from    get_data    import get_news, get_tweets
-
-# API_KEY_1   = os.environ.get("API_KEY_1")
-# API_NAME_1  = os.environ.get("API_NAME_1")
-
-# API_KEY_2   = os.environ.get("API_KEY_2")
-# API_NAME_2  = os.environ.get("API_NAME_2")
-
-# @app.route('/')
-# def get_index()():
-#     articles  = get_news(API_NAME_2, API_KEY_2)
-#     tweets    = get_tweets(API_NAME_1, API_KEY_1) 
-#     return render_template('mpa.html', tweets = tweets, articles = articles)
-    
-    
-# ------------------------------------------------------------------------------
-
 if __name__ == '__main__':
     app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 8080)))
-
-# ------------------------------------------------------------------------------
